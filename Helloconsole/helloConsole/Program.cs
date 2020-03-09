614e214b8e7a69d451ae8d9dfb19b462ca74dafb
Jérémie <jeremie.pichot.pro@gmail.com>
2020-03-09 18:20:10 +0100

    1er commit du projet HelloConsole

---
+++ Helloconsole/helloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
